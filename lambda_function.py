39b9acd2b12f4226c8d2dd21ebb404ee994674aa
Ngoc7-psu <59721719+Ngoc7-psu@users.noreply.github.com>
2020-12-01 22:54:04 -0500

    Add files via upload
    Third commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,192 @@
+
 import json
 
-def lambda_handler(event, context):
-    # TODO implement
+"""
+This is a Python code for Alexa to get you building skills (conversations) quickly.
+"""
+
+from __future__ import print_function
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
     return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': "SessionSpeechlet - " + title,
+            'content': "SessionSpeechlet - " + output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
     }
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+def get_patientRefill_response():
+    """Patient refill reminder. Make sure to add this intent
+    in your alexa skill in order for it to work.
+    """
+    session_attributes = {}
+    card_title = "Patient refil reminder"
+    speech_output = "This is a reminder, refill your medication"
+    reprompt_text = "This is a reminder, refill your medication"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+#------------------------------------------------------------------------------        
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+    session_attributes = {}
+    card_title = "Welcome to the Jeager medicine management system"
+    speech_output = "Welcome to Jeager medicine!"
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "I don't know if you heard me, welcome to Jeager medicine!"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+#-------------------------------------------------------------------------------
+
+def get_caretaker_notification_response():
+    """Caretaker notification. Recieves user input, and replies with speech otput."""
+    
+    session_attributes = {}
+    card_title = "Caretaker Notification"
+    speech_output = "This is a Notification, remind patient to take their medication"
+    reprompt_text = "This is a Notification, remind patient to take their medication"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+#-------------------------------------------------------------------------------        
+        
+def get_patient_reminder_response():
+    """Patient Reminder. Takes user input and returns speech output."""
+    
+    session_attributes = {}
+    card_title = "Patient reminder"
+    speech_output = "Heres your reminder, Josh has not taken his diabeties medication."
+    reprompt_text = "Heres your reminder, Josh has not taken  diabeties medication."
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+        
+#-------------------------------------------------------------------------------        
+        
+def get_caretaker_refill_response():
+    """Caretaker refill reminder. takes user input and returns speech output."""
+    
+    session_attributes = {}
+    card_title = "Caretaker refill reminder"
+    speech_output = "This is a reminder, remind patient to refill their medication"
+    reprompt_text = "This is a reminder, remind patient to refill their medication"
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+    
+#-------------------------------------------------------------------------------    
+    
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Thank you for using the Jeager medicine management system. " \
+                    "Have a nice day! "
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts.
+        One possible use of this function is to initialize specific 
+        variables from a previous state stored in an external database
+    """
+    # Add additional code here as needed
+    pass
+
+    
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+    # Dispatch to your skill's launch message
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "test":
+        return get_test_response()
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("Incoming request...")
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
